5bfa39ff2e2d9d2be2bed6e6e3e0ab3dd13d36b7
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-06-18 16:24:10 -0300

    Add Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -113,6 +113,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.showAndWait();
 
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
