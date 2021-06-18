c6d31ece283ad4f2ac30c3603eb4becac9b2c186
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-06-18 10:44:30 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -22,4 +22,12 @@ public class DepartmentService {
 		//DataBase
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		} else {
+			dao.update(obj);
+		}
+	}
 }
